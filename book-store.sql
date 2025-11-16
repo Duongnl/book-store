1d4350ad0683632e64cd80100b3e199670e6e1e6
quanglinh <116327425+Linh1023@users.noreply.github.com>
2025-11-16 18:36:27 +0700

    upload database

--- book-store.sql
+++ book-store.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 16, 2025 lúc 11:05 AM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2025 at 12:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `book-store`
+-- Database: `book-store`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `cart`
+-- Table structure for table `cart`
 --
 
 CREATE TABLE `cart` (
@@ -35,17 +35,18 @@ CREATE TABLE `cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `cart`
+-- Dumping data for table `cart`
 --
 
 INSERT INTO `cart` (`id`, `quantity`, `product_id`, `user_id`) VALUES
-(9, 1, 6, 1),
-(10, 1, 7, 1);
+(4, 1, 18, 4),
+(9, 1, 7, 1),
+(10, 1, 13, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -56,20 +57,20 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `image_name`, `is_active`, `name`) VALUES
-(1, 'https://cdn0.f...thanh_ly.jpg', b'1', 'Science Fiction'),
-(2, 'https://cdn0.f...1_52.jpg', b'1', 'English Learning'),
-(3, 'https://cdn0.f...du-lieu.jpg', b'1', 'Science'),
-(4, 'https://cdn0.f...392435.jpg', b'1', 'Technology'),
-(5, 'https://cdn0.f...916470.jpg', b'1', 'Education');
+(1, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935244866957_thanh_ly.jpg', b'1', 'Science Fiction'),
+(2, 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_244718_1_52.jpg', b'1', 'English Learning'),
+(3, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 'Science'),
+(4, 'https://cdn0.fahasa.com/media/catalog/product/9/7/9786040392435.jpg', b'1', 'Technology'),
+(5, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935280916470.jpg', b'1', 'Education');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `order_address`
+-- Table structure for table `order_address`
 --
 
 CREATE TABLE `order_address` (
@@ -85,26 +86,22 @@ CREATE TABLE `order_address` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `order_address`
+-- Dumping data for table `order_address`
 --
 
 INSERT INTO `order_address` (`id`, `address`, `city`, `email`, `first_name`, `last_name`, `mobile_no`, `pincode`, `state`) VALUES
-(1, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GVap'),
-(2, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GVap'),
-(3, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GVap'),
+(1, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GoVap'),
+(2, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GoVap'),
+(3, 'Address', 'HCMCity', 'baothong15082003@gmail.com', 'Dang', 'Thong', '1211221121', '111111', 'GoVap'),
 (4, 'Admin Address', 'AdminCity', 'admin@admin.com', 'Admin', 'Admin', '0000000000', '000000', 'Admin State'),
 (5, 'Admin Address', 'AdminCity', 'admin@admin.com', 'Admin', 'Admin', '0000000000', '000000', 'Admin State'),
 (6, 'Admin Address', 'AdminCity', 'admin@admin.com', 'Admin', 'Thong', '0000000000', '000000', 'Admin State'),
-(7, 'Admin Address', 'AdminCity', 'admin@admin.com', 'Admin', 'Thong', '0000000000', '000000', 'Admin State'),
-(8, 'User Address 1', 'Hanoi', 'user1@example.com', 'đs', 'dsads', '0123456789', '100000', 'Dong Da'),
-(9, 'User Address 1', 'Hanoi', 'user1@example.com', 'đs', 'dsads', '0123456789', '100000', 'Dong Da'),
-(10, 'User Address 1', 'Hanoi', 'user1@example.com', 'duong', 'le', '0123456789', '100000', 'Dong Da'),
-(11, 'User Address 1', 'Hanoi', 'user1@example.com', 'duong', 'le', '0123456789', '100000', 'Dong Da');
+(7, 'Admin Address', 'AdminCity', 'admin@admin.com', 'Admin', 'Thong', '0000000000', '000000', 'Admin State');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `product`
+-- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
@@ -112,29 +109,39 @@ CREATE TABLE `product` (
   `description` varchar(5000) DEFAULT NULL,
   `discount` int(11) NOT NULL,
   `discount_price` double DEFAULT NULL,
-  `image_url` varchar(1000) DEFAULT NULL,
+  `image` varchar(1000) DEFAULT NULL,
   `is_active` bit(1) DEFAULT NULL,
   `price` double DEFAULT NULL,
   `stock` int(11) NOT NULL,
   `title` varchar(500) DEFAULT NULL,
-  `category_id` int(11) DEFAULT NULL,
-  `image` varchar(1000) DEFAULT NULL
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `product`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `description`, `discount`, `discount_price`, `image_url`, `is_active`, `price`, `stock`, `title`, `category_id`, `image`) VALUES
-(6, 'The term \"artificial intelligence\"...', 10, 539100, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 599000, 49, 'AI 2041 - For The Future', 4, NULL),
-(7, 'Data science is a new artificial ecosystem...', 5, 332500, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 350000, 19, 'Data Science', 3, NULL),
-(20, 'Maya And The Robot', 0, 156600, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 156600, 0, 'Maya And The Robot', 1, NULL),
-(21, 'vip lắm product', 0, 25000, NULL, b'1', 25000, 66, 'dữ liệu vip', 1, 'Spring-BOOT-1.webp');
+INSERT INTO `product` (`id`, `description`, `discount`, `discount_price`, `image`, `is_active`, `price`, `stock`, `title`, `category_id`) VALUES
+(6, 'The term \"artificial intelligence\" was proposed much earlier than we know - in the summer of 1956, by computer scientist John McCarthy at the Dartmouth Conference.', 10, 539100, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935244866957_thanh_ly.jpg', b'1', 599000, 50, 'AI 2041 - For The Future', 4),
+(7, 'Data science is a new artificial ecosystem in the modern information age, ranging from crime detection to disease prediction. But did you know it\'s not just about the massive amount of information collected by computers, smartphones, and credit cards?', 5, 332500, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 350000, 23, 'Data Science', 3),
+(8, 'Dive into the fascinating world of quantum mechanics with this easy-to-understand book.', 10, 269100, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 299000, 20, 'Quantum Physics Explained', 3),
+(9, 'Understand the fundamentals of artificial intelligence and its applications in real-world problems.', 12, 351120, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935244866957_thanh_ly.jpg', b'1', 399000, 35, 'Learning AI Basics', 4),
+(10, 'A comprehensive guide to start learning English effectively, with plenty of examples and exercises.', 15, 170000, 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_244718_1_52.jpg', b'1', 200000, 49, 'English for Beginners', 2),
+(11, 'This book covers advanced concepts in electronics technology for engineering students.', 18, 451000, 'https://cdn0.fahasa.com/media/catalog/product/9/7/9786040392435.jpg', b'1', 550000, 30, 'Advanced Electronics', 4),
+(12, 'An excellent resource for educators to improve their teaching methods.', 5, 285000, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935280916470.jpg', b'1', 300000, 39, 'Teaching Strategies', 5),
+(13, 'Explore the mysteries of the cosmos with this thrilling science book.', 8, 386400, 'https://cdn0.fahasa.com/media/catalog/product/3/0/30-giay-khoa-hoc_khoa-hoc-du-lieu.jpg', b'1', 420000, 25, 'Physics of the Universe', 3),
+(14, 'Discover how AI is transforming industries and creating new possibilities.', 10, 441000, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935244866957_thanh_ly.jpg', b'1', 490000, 20, 'Innovating with AI', 4),
+(15, 'Improve your English communication skills for daily conversations.', 12, 158400, 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_244718_1_52.jpg', b'1', 180000, 45, 'Practical English Skills', 2),
+(16, 'A beginner-friendly introduction to designing electrical circuits.', 20, 280000, 'https://cdn0.fahasa.com/media/catalog/product/9/7/9786040392435.jpg', b'1', 350000, 29, 'Basic Circuits Design', 4),
+(17, 'A look at modern trends and challenges in education today.', 10, 243000, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935280916470.jpg', b'1', 270000, 54, 'Modern Education Trends', 5),
+(18, 'Destination B1 - Grammar And Vocabulary With Answer Key', 0, 270000, 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935246945001.jpg', b'1', 270000, 997, 'Destination B1 - Grammar And Vocabulary With Answer Key', 2),
+(19, 'The Witches', 0, 191700, 'https://cdn0.fahasa.com/media/catalog/product/9/7/9780241677667.jpg', b'1', 191700, 5, 'The Witches', 1),
+(20, 'Maya And The Robot', 0, 156600, 'https://cdn0.fahasa.com/media/catalog/product/9/7/9781984814654.jpg', b'1', 156600, 5, 'Maya And The Robot', 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `product_order`
+-- Table structure for table `product_order`
 --
 
 CREATE TABLE `product_order` (
@@ -151,26 +158,22 @@ CREATE TABLE `product_order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `product_order`
+-- Dumping data for table `product_order`
 --
 
 INSERT INTO `product_order` (`id`, `order_date`, `order_id`, `payment_type`, `price`, `quantity`, `status`, `order_address_id`, `product_id`, `user_id`) VALUES
-(1, '2024-10-28 19:39:56.787062', 'fc4288d1...', 'COD', 332500, 1, 'Delivered', 1, 6, 3),
-(2, '2024-11-28 19:39:56.803148', 'd2e9fab1...', 'COD', 285000, 1, 'Delivered', 1, 7, 3),
-(3, '2024-10-28 19:40:59.338824', 'be0edd11...', 'COD', 270000, 2, 'Delivered', 1, 6, 2),
-(4, '2024-11-28 19:54:46.760824', '51ccfea3...', 'COD', 332500, 1, 'Delivered', 1, 7, 1),
-(5, '2024-11-28 19:54:46.779555', 'b17d1599...', 'COD', 170000, 1, 'Delivered', 1, 20, 1),
-(6, '2024-11-28 19:55:25.445009', '92c46d40...', 'COD', 280000, 1, 'Order Received', 1, 6, 1),
-(7, '2024-11-28 19:55:25.449993', 'd93c575e...', 'COD', 243000, 1, 'Order Received', 1, 7, 1),
-(8, '2025-11-16 16:47:32.000000', '5fd6131f-0ef8-445f-a535-2d13e85dec15', 'ONLINE', 156600, 4, 'In Progress', 8, 20, 2),
-(9, '2025-11-16 16:47:32.000000', 'ab5f2179-fac3-4801-a69d-c8ccf31b4b77', 'ONLINE', 539100, 1, 'In Progress', 9, 6, 2),
-(10, '2025-11-16 16:49:09.000000', '6ba76085-8a11-4cf2-91b1-a85735832cea', 'COD', 332500, 4, 'In Progress', 10, 7, 2),
-(11, '2025-11-16 16:56:23.000000', '66d39c0b-163d-48a7-b171-cc3560dd4ec0', 'COD', 156600, 1, 'In Progress', 11, 20, 2);
+(1, '2024-10-28 19:39:56.000000', 'fc4288d1-5f8a-4878-a055-d8d10604bc89', 'COD', 332500, 1, 'Delivered', 1, 7, 4),
+(2, '2024-11-28 19:39:56.000000', 'd2e9fab1-f1f8-420d-a0ca-7c8a6cbd34c6', 'COD', 285000, 1, 'Delivered', 2, 12, 4),
+(3, '2024-10-28 19:40:59.000000', 'be0edd11-f100-4e8e-9e72-8b065f3ac897', 'COD', 270000, 2, 'Delivered', 3, 18, 4),
+(4, '2024-11-28 19:54:46.000000', '51ccfea3-ac59-4edd-884f-638c6a7e9202', 'COD', 332500, 1, 'Delivered', 4, 7, 1),
+(5, '2024-11-28 19:54:46.000000', 'b17d1599-536b-4eb0-aa86-fcf34fbe9b52', 'COD', 170000, 1, 'Delivered', 5, 10, 1),
+(6, '2024-11-28 19:55:25.000000', '92c46d40-f085-4bb8-9662-e97e50c1dac3', 'COD', 280000, 1, 'Order Received', 6, 16, 1),
+(7, '2024-11-28 19:55:25.000000', 'd93c575e-7968-4c22-a3ae-65424f999f39', 'COD', 243000, 1, 'Order Received', 7, 17, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `user_dtls`
+-- Table structure for table `user_dtls`
 --
 
 CREATE TABLE `user_dtls` (
@@ -193,124 +196,125 @@ CREATE TABLE `user_dtls` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `user_dtls`
+-- Dumping data for table `user_dtls`
 --
 
 INSERT INTO `user_dtls` (`id`, `account_non_locked`, `address`, `city`, `email`, `failed_attempt`, `is_enable`, `lock_time`, `mobile_number`, `name`, `password`, `pincode`, `profile_image`, `reset_token`, `role`, `state`) VALUES
-(1, b'1', 'Admin Address', 'AdminCity', 'admin@admin.com', 1, b'1', NULL, '0000000000', 'Administrator', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '000000', 'default.jpg', NULL, 'ROLE_ADMIN', 'Admin State'),
-(2, b'1', 'User Address 1', 'Hanoi', 'user1@example.com', 0, b'1', NULL, '0123456789', 'User One', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '100000', 'default.jpg', NULL, 'ROLE_USER', 'Dong Da'),
-(3, b'1', 'User Address 2', 'Hanoi', 'user2@example.com', 0, b'1', NULL, '0987654321', 'User Two', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '100000', 'default.jpg', NULL, 'ROLE_USER', 'Ba Dinh');
+(1, b'1', 'Admin Address', 'AdminCity', 'admin@example.com', 2, b'1', NULL, '0000000000', 'Administrator', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '000000', 'default.jpg', NULL, 'ROLE_ADMIN', 'Admin State'),
+(2, b'1', 'User Address 1', 'Hanoi', 'user@example.com', 0, b'1', NULL, '0123456789', 'User One', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '100000', 'default.jpg', NULL, 'ROLE_USER', 'Dong Da'),
+(3, b'1', 'User Address 2', 'Hanoi', 'user2@example.com', 0, b'1', NULL, '0987654321', 'User Two', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '100000', 'default.jpg', NULL, 'ROLE_USER', 'Ba Dinh'),
+(4, b'1', 'Address', 'HCMCity', 'baothong15082003@gmail.com', 0, b'1', NULL, '1211221121', 'BaoThong', '$2a$10$iSikT1CN8QevmEoucm7lqetr5lYhhSF9BQ.Q1nMmDS/cV0ad/SVpK', '111111', 'Java-programming.png', NULL, 'ROLE_USER', 'GoVap');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `cart`
+-- Indexes for table `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_cart_product` (`product_id`),
-  ADD KEY `FK_cart_user` (`user_id`);
+  ADD KEY `FK3d704slv66tw6x5hmbm6p2x3u` (`product_id`),
+  ADD KEY `FK9x4wn098i53ikun1ynxet2ynj` (`user_id`);
 
 --
--- Chỉ mục cho bảng `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `order_address`
+-- Indexes for table `order_address`
 --
 ALTER TABLE `order_address`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `product`
+-- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_product_category` (`category_id`);
+  ADD KEY `FK1mtsbur82frn64de7balymq9s` (`category_id`);
 
 --
--- Chỉ mục cho bảng `product_order`
+-- Indexes for table `product_order`
 --
 ALTER TABLE `product_order`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_order_address` (`order_address_id`),
-  ADD KEY `FK_order_product` (`product_id`),
-  ADD KEY `FK_order_user` (`user_id`);
+  ADD UNIQUE KEY `UK_qcdbxaeuc7c5gahwh0dutg04r` (`order_address_id`),
+  ADD KEY `FKh73acsd9s5wp6l0e55td6jr1m` (`product_id`),
+  ADD KEY `FK4f2ycr32kigtux5ag3tv0xu5m` (`user_id`);
 
 --
--- Chỉ mục cho bảng `user_dtls`
+-- Indexes for table `user_dtls`
 --
 ALTER TABLE `user_dtls`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `cart`
+-- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT cho bảng `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT cho bảng `order_address`
+-- AUTO_INCREMENT for table `order_address`
 --
 ALTER TABLE `order_address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT cho bảng `product`
+-- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT cho bảng `product_order`
+-- AUTO_INCREMENT for table `product_order`
 --
 ALTER TABLE `product_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT cho bảng `user_dtls`
+-- AUTO_INCREMENT for table `user_dtls`
 --
 ALTER TABLE `user_dtls`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `cart`
+-- Constraints for table `cart`
 --
 ALTER TABLE `cart`
-  ADD CONSTRAINT `FK_cart_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  ADD CONSTRAINT `FK_cart_user` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`);
+  ADD CONSTRAINT `FK3d704slv66tw6x5hmbm6p2x3u` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `FK9x4wn098i53ikun1ynxet2ynj` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`);
 
 --
--- Các ràng buộc cho bảng `product`
+-- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `FK_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `FK1mtsbur82frn64de7balymq9s` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 --
--- Các ràng buộc cho bảng `product_order`
+-- Constraints for table `product_order`
 --
 ALTER TABLE `product_order`
-  ADD CONSTRAINT `FK_order_address` FOREIGN KEY (`order_address_id`) REFERENCES `order_address` (`id`),
-  ADD CONSTRAINT `FK_order_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  ADD CONSTRAINT `FK_order_user` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`);
+  ADD CONSTRAINT `FK4f2ycr32kigtux5ag3tv0xu5m` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`),
+  ADD CONSTRAINT `FK8frxalwc79tpxo7hgqp3hsjck` FOREIGN KEY (`order_address_id`) REFERENCES `order_address` (`id`),
+  ADD CONSTRAINT `FKh73acsd9s5wp6l0e55td6jr1m` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
